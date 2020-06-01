b83de0193ff7f70c5d7b7fde55006a50f7eea3a8
rzarebapwr <238657@student.pwr.edu.pl>
2020-06-01 12:05:00 +0200

    Base58 added, Ripemd160 added - to clean up

--- blockchains/trai-blockchain/src/main/java/com/trai/blockchain/App.java
+++ blockchains/trai-blockchain/src/main/java/com/trai/blockchain/App.java
@@ -2,11 +2,13 @@ package com.trai.blockchain;
 
 import com.trai.blockchain.cryptography.*;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
@@ -20,9 +22,40 @@ import java.util.List;
 
 public class App {
 
+    public static final String base58Chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static byte[] hexToBytes(String s) {
+        if (s.length() % 2 != 0)
+            throw new IllegalArgumentException();
+
+        byte[] b = new byte[s.length() / 2];
+        for (int i = 0; i < s.length(); i += 2) {
+            if (s.charAt(i) == '+' || s.charAt(i) == '-')
+                throw new IllegalArgumentException();
+            b[i / 2] = (byte)Integer.parseInt(s.substring(i, i + 2), 16);
+        }
+        return b;
+    }
+
     public static BigInteger sha256(String message) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] hash = digest.digest(message.getBytes(StandardCharsets.UTF_8));
+        return new BigInteger(hash);
+    }
+
+    public static BigInteger ripemd160(String message) {
+        byte[] hash = Ripemd160.getHash(message.getBytes(StandardCharsets.UTF_8));
         return new BigInteger(hash);
     }
 
@@ -42,6 +75,7 @@ public class App {
     }
 
     public static String reverseHex(String hexNum) {
+        /* Change endianess */
         if (!(hexNum.length() % 2 == 0))
             hexNum = "0" + hexNum;
 
@@ -55,8 +89,7 @@ public class App {
         return new String(chars);
     }
 
-
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 
 
 
@@ -70,10 +103,23 @@ public class App {
         BigInteger messageToCheck = sha256("Hello2");
         boolean verified = ECDSA.verify(messageToCheck, publicKey, sig);
         String der = sig.toDER();
-        System.out.println(der);
+//        System.out.println(der);
 
 
+//        System.out.println(toBase58("1ff00"));
+//        System.out.println(Base58.encode("1ff00".getBytes()));
+//        System.out.println(Base58.encode("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838ad3".getBytes()));
+//        System.out.println(Base58.encode(new BigInteger("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d", 16).toByteArray()));
+//        System.out.println(Base58.encode(new BigInteger("eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c", 16).toByteArray()));
+//        System.out.println(Base58.encode(new BigInteger("c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6", 16).toByteArray()));
 
+//        System.out.println(ripemd160("Hello").toString(16));
+        byte[] hash = Ripemd160.getHash("abc".getBytes("US-ASCII"));
+        BigInteger rip160 = new BigInteger(hash);
+//        System.out.println(rip160.toString(16));
+        System.out.println(bytesToHex(hash));
+//        System.out.println(rip160.toString(16));
+//        System.out.println(sha256("Hello").toString(16));
 
     }
 
